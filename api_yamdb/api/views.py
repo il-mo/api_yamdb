1e83e474120cf0426bfc4cb6688cfa8866d3e647
Ilya Moiseev <ilya.moiseev95@gmail.com>
2021-10-05 20:53:56 +0300

    05.10.2021

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from reviews.models import Comment, Review, Titles
+from rest_framework import filters, mixins, permissions, viewsets
+# from rest_framework.pagination import LimitOffsetPagination
+# from django_filters.rest_framework import DjangoFilterBackend
+#
+# from .permissions import IsAuthorOrReadOnlyPermission
+from .serializers import (
+    ReviewSerializer,
+    CommentSerializer,
 
-# Create your views here.
+)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    # permission_classes = [IsAuthorOrReadOnlyPermission]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, id=self.kwargs['review_id'])
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, id=self.kwargs['review_id'])
+        serializer.save(author=self.request.user, review=review)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'api',
+    'reviews',
+    'users',
 ]
 
 MIDDLEWARE = [
