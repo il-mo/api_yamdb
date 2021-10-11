92bfaf708fe8b407b25b8b50d0c6174b2caeb7b8
David Kuenka <cuencaldd2001.29@gmail.com>
2021-10-11 20:50:52 +0300

    flake fix

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated
-            and request.user.is_admin)
+                and request.user.is_admin)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
